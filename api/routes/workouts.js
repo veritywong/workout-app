d5720444774bad7dc7d9127cf77d298129e96e66
Verity <veritywong90@gmail.com>
2023-08-01 16:34:26 +0100

    CRUD controllers added

--- api/routes/workouts.js
+++ api/routes/workouts.js
@@ -1,29 +1,26 @@
 const express = require('express')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({mssg: 'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 // GET a single workout
-router.get('/:id', (req, res) =>{
-    res.json({mssg: 'GET a single workout'})
-})
+router.get('/:id', getWorkout)
 
 //POST a new workout
-router.post('/', (req, res) => {
-    res.json({messg: 'POST a new workout'})
-})
+router.post('/', createWorkout)
 
 //DELETE a new workout
-router.delete('/:id', (req, res) => {
-    res.json({messg: 'DELETE a new workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 //UPDATE a new workout
-router.patch('/:id', (req, res) => {
-    res.json({messg: 'UPDATE a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
